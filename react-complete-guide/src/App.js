45773f63b090502ce7bf7dfff19984114b58407c
k-e-r <damajonboua@gmail.com>
2022-02-15 08:27:30 -0800

    add/divide ExpenseDate

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,23 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
     </div>
   );
 }
