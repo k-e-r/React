c4621ad0e3d17d9c498403b619efab94a0a0823f
k-e-r <damajonboua@gmail.com>
2022-04-01 23:41:40 -0700

    modify(09)

--- 09-working-with-forms-and-user-input/src/components/SimpleInput.js
+++ 09-working-with-forms-and-user-input/src/components/SimpleInput.js
@@ -1,11 +1,19 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+
+  nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
   return (
     <form>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
