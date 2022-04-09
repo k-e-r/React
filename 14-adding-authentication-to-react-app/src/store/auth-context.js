ba03948a8dccca7b07e25d7455c2278704766168
k-e-r <damajonboua@gmail.com>
2022-04-08 20:10:57 -0700

    add calculating remaining time(login)

--- 14-adding-authentication-to-react-app/src/store/auth-context.js
+++ 14-adding-authentication-to-react-app/src/store/auth-context.js
@@ -7,6 +7,15 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingDuration = adjExpirationTime - currentTime;
+
+  return remainingDuration;
+};
+
 export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem('token');
   const [token, setToken] = useState(initialToken);
@@ -14,14 +23,18 @@ export const AuthContextProvider = (props) => {
   // 文字列をtrue/false変換するために２重(!)使用
   const userIsLoggedIn = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem('token', token);
-  };
-
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem('token');
+  };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem('token', token);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+
+    setTimeout(logoutHandler, remainingTime);
   };
 
   const contextValue = {
