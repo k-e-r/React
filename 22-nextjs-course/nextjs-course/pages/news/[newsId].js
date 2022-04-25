98593c769603262256fc05a3ce904a14d8a4b409
k-e-r <damajonboua@gmail.com>
2022-04-25 03:46:13 -0700

    add extracting dynamic parameter values

--- 22-nextjs-course/nextjs-course/pages/news/[newsId].js
+++ 22-nextjs-course/nextjs-course/pages/news/[newsId].js
@@ -1,6 +1,12 @@
+import { useRouter } from 'next/router';
+
 // our-domin.com/news/something-important.js
 
 function DetailPage() {
+  const router = useRouter();
+
+  console.log(router.query.newsId);
+
   return <h1>The Detail Page</h1>;
 }
 
