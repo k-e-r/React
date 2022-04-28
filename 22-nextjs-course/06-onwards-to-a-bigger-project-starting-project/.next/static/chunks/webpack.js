68777c367050ded510d182c4a14b011942b17fc7
k-e-r <damajonboua@gmail.com>
2022-04-27 20:07:30 -0700

    add sending http request to our api routes

--- 22-nextjs-course/06-onwards-to-a-bigger-project-starting-project/.next/static/chunks/webpack.js
+++ 22-nextjs-course/06-onwards-to-a-bigger-project-starting-project/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "297f4dcac00e12296c54";
+/******/ 	var hotCurrentHash = "5401e5f4f2b75afed419";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
