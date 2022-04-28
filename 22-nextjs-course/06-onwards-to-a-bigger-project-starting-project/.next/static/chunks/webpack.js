9a24e9c4810f9a5e6ea3052d4eca61a6ccd59721
k-e-r <damajonboua@gmail.com>
2022-04-27 20:35:49 -0700

    add getting meetup details data & preparing pages

--- 22-nextjs-course/06-onwards-to-a-bigger-project-starting-project/.next/static/chunks/webpack.js
+++ 22-nextjs-course/06-onwards-to-a-bigger-project-starting-project/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ce3a94bb2c46db23e261";
+/******/ 	var hotCurrentHash = "e0234d80565ab7566674";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
